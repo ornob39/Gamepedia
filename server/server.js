56925249d96b991379951a364094c4de7c43c46f
MD. Safayet Islam <ornob39@gmail.com>
2021-06-08 04:26:53 +0600

    Optimization

--- server/server.js
+++ server/server.js
@@ -3,19 +3,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const api = require('./routes/api')
 const PORT = 3000;
+const path = require('path');
 
 const app = express();
+const http = require('http').createServer(app)
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api', api);
-
-
-app.get('/', function(req, res) {
-    res.send('Hello From The server')
-})
-
-app.listen(PORT, function() {
-    console.log('Server running on localhost: ' + PORT)
-})
+const HTML_DIR = path.join(__dirname, '/public/')
+app.use(express.static(HTML_DIR))
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,22 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { HttpService } from 'src/app/services/http.service';
-import { Game } from 'src/app/model';
+
+import { Router } from '@angular/router';
+
 import { AuthService } from '../auth.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit {
-  constructor(
-    private httpService: HttpService,
-    private router: Router,
-    public _authService: AuthService,
-    private _snackBar: MatSnackBar
-  ) {}
+  constructor(private router: Router, public _authService: AuthService) {}
 
   ngOnInit(): void {}
 
@@ -46,11 +40,36 @@ export class HomepageComponent implements OnInit {
   ];
 
   best_titles = [
-    'Cyberpunk',
-    'outriders',
-    'halo-infinite',
-    'resident-evil-village',
-    'stalker-2',
+    'cyberpunk-2077',
+    'biomutant',
+    'hitman-3',
+    'far-cry-6',
+    'ghostwire-tokio',
+  ];
+
+  slides_10 = [
+    { image: '../../assets/Homepage/Slides/top10/1.jpg' },
+    { image: '../../assets/Homepage/Slides/top10/2.jpg' },
+    { image: '../../assets/Homepage/Slides/top10/3.jpg' },
+    { image: '../../assets/Homepage/Slides/top10/4.jpg' },
+    { image: '../../assets/Homepage/Slides/top10/5.jpg' },
+    { image: '../../assets/Homepage/Slides/top10/6.jpg' },
+    { image: '../../assets/Homepage/Slides/top10/7.jpg' },
+    { image: '../../assets/Homepage/Slides/top10/8.jpg' },
+    { image: '../../assets/Homepage/Slides/top10/9.jpg' },
+    { image: '../../assets/Homepage/Slides/top10/10.jpg' },
+  ];
+  top10_titles = [
+    'the-witcher-3-wild-hunt',
+    'the-last-of-us',
+    'the-legend-of-zelda-breath-of-the-wild',
+    'red-dead-redemption-2',
+    'god-of-war-2',
+    'uncharted-4-a-thiefs-end',
+    'half-life-2',
+    'grand-theft-auto-v',
+    'mafia-the-city-of-lost-heaven',
+    'dark-souls-iii',
   ];
 
   //Navigate to details
